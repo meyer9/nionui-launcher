083912d4ca6b12c039873f7e3f85c077cb3da934
Julian Meyer <julianmeyer2000@gmail.com>
2017-02-20 21:00:38 -0800

    Update g++ in travis

--- travis-build.sh
+++ travis-build.sh
@@ -11,6 +11,8 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]] && [[ "$BUILD_SELF" == "1" ]]; then
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ "$BUILD_SELF" == "1" ]]; then
+  sudo unlink /usr/bin/g++ && sudo ln -s /usr/bin/g++-5 /usr/bin/g++
+  gcc --version
   wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
